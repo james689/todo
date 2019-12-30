aa39dc93b02e4f7cd671f8345c14d827b747beca
James <james689@users.noreply.github.com>
2019-12-30 16:10:08 +0000

    Add List class and functionality to name/rename a list.

---
+++ src/main/java/core/List.java
@@ -0,0 +1,18 @@
+
+package core;
+
+public class List {
+    private String name; // the name of this list
+    
+    public List(String name) {
+        this.name = name;
+    }
+    
+    public String getName() { 
+        return name; 
+    }
+    
+    public void setName(String name) { 
+        this.name = name;
+    }
+}
--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -3,6 +3,11 @@ package core;
 
 public class Main {
     public static void main(String[] args) {
-        
+        List chores = new List("chores");
+        System.out.println("chores.getName() = " + chores.getName());
+        chores.setName("shopping");
+        System.out.println("chores.getName() = " + chores.getName());
+        chores.setName("biscuits");
+        System.out.println("chores.getName() = " + chores.getName());
     }
 }
